84e7b3eecf8906766834a8272f0ff40f2896732d
viiiviii538 <viiiviii538@gmail.com>
2025-07-05 15:21:11 +0900

    Update app title to NWCD across platforms

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "nwc_densetsu");
+    gtk_header_bar_set_title(header_bar, "NWCD");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "nwc_densetsu");
+    gtk_window_set_title(window, "NWCD");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
