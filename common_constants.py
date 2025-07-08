426919f281c3757fe064951dd2dd47b4fc2a9ae3
viiiviii538 <viiiviii538@gmail.com>
2025-07-08 10:59:24 +0900

    Add color constants and update imports

--- common_constants.py
+++ common_constants.py
@@ -3,3 +3,7 @@
 DANGER_COUNTRIES = {"RU", "CN", "KP"}  # Russia, China, North Korea
 
 SAFE_COUNTRIES = {"JP", "US", "GB", "DE", "FR", "CA", "AU"}
+
+# ANSI color escape sequences for console output.
+RED = "\033[31m"
+RESET = "\033[0m"
