3907907b0aa589b10a7bb8ac616a12d31aeef97a
viiiviii538 <viiiviii538@gmail.com>
2025-07-09 22:43:41 +0900

    Update scoring weights and add new risk metrics

--- test/test_security_score.py
+++ test/test_security_score.py
@@ -18,7 +18,7 @@ class CalcSecurityTest(unittest.TestCase):
     def test_single_high(self):
         res = calc_security_score({"danger_ports": ["3389"]})
         self.assertEqual(res["high_risk"], 1)
-        self.assertAlmostEqual(res["score"], 9.3, places=1)
+        self.assertAlmostEqual(res["score"], 9.0, places=1)
 
     def test_mixed_levels(self):
         data = {"danger_ports": 1, "ssl": "invalid", "open_port_count": 2}
@@ -26,8 +26,19 @@ class CalcSecurityTest(unittest.TestCase):
         self.assertEqual(res["high_risk"], 2)
         self.assertEqual(res["medium_risk"], 0)
         self.assertEqual(res["low_risk"], 1)
-        expected = 10 - 1.4 - 0.2
+        expected = 10 - 2 * HIGH_WEIGHT - 1 * LOW_WEIGHT
         self.assertAlmostEqual(res["score"], expected, places=1)
+
+    def test_new_metrics(self):
+        data = {
+            "firewall_enabled": False,
+            "defender_enabled": False,
+            "os_version": "Windows XP",
+            "smbv1": True,
+        }
+        res = calc_security_score(data)
+        self.assertEqual(res["high_risk"], 4)
+        self.assertEqual(res["score"], 6.0)
 
 
 if __name__ == "__main__":
