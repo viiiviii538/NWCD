1594156b68c7d913c4c5fbbd91094482cc19158c
viiiviii538 <viiiviii538@gmail.com>
2025-07-11 22:49:42 +0900

    Make UI monochrome by default

--- lib/config.dart
+++ lib/config.dart
@@ -1,2 +1,7 @@
-const bool useColor = bool.fromEnvironment('NWCD_USE_COLOR', defaultValue: true);
+/// Determines if the UI should be rendered in color.
+///
+/// The default is set to `false` so the application runs in monochrome unless
+/// explicitly overridden via the `NWCD_USE_COLOR` environment variable.
+const bool useColor =
+    bool.fromEnvironment('NWCD_USE_COLOR', defaultValue: false);
 
