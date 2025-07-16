40ecfbda4020f8b88f0083256bf8618f2ad59eff
viiiviii538 <viiiviii538@gmail.com>
2025-07-16 12:42:48 +0900

    Update score_chart_test.dart

--- test/score_chart_test.dart
+++ test/score_chart_test.dart
@@ -6,13 +6,13 @@ import 'package:nwc_densetsu/diagnostics.dart';
 void main() {
   testWidgets('ScoreChart renders', (WidgetTester tester) async {
     const reports = [
-      const SecurityReport('1.1.1.1', 9.0, const <RiskItem>[], const <String>[], '',
-          openPorts: const [80], geoip: 'US', utmActive: false),
-      const SecurityReport('2.2.2.2', 3.0, const <RiskItem>[], const <String>[], '',
-          openPorts: const [22], geoip: 'JP', utmActive: false),
+      SecurityReport('1.1.1.1', 9.0, <RiskItem>[], [], '',
+          openPorts: [80], geoip: 'US',utmActive: false,),
+      SecurityReport('2.2.2.2', 3.0, <RiskItem>[], [], '',
+          openPorts: [22], geoip: 'JP',utmActive: false,),
     ];
     await tester.pumpWidget(
-      MaterialApp(home: Scaffold(body: ScoreChart(reports: reports))),
+      const MaterialApp(home: Scaffold(body: ScoreChart(reports: reports))),
     );
     expect(find.byType(ScoreChart), findsOneWidget);
   });
