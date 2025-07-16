e65be146fa9e31c888f9b9536d410dc50d49db4c
viiiviii538 <viiiviii538@gmail.com>
2025-07-16 12:42:38 +0900

    Update diagnostic_result_page_test.dart

--- test/diagnostic_result_page_test.dart
+++ test/diagnostic_result_page_test.dart
@@ -13,13 +13,13 @@ void main() {
     ];
 
     const summaries = [
-      const PortScanSummary('1.1.1.1', [
+      PortScanSummary('1.1.1.1', [
         PortStatus(445, 'closed', 'smb'),
         PortStatus(3389, 'open', 'rdp'),
       ])
     ];
     await tester.pumpWidget(
-      MaterialApp(
+      const MaterialApp(
         home: DiagnosticResultPage(
           securityScore: 2.0,
           items: items,
@@ -80,8 +80,7 @@ void main() {
           vendor: 'Acme',
           name: 'Dev',
           status: 'ok',
-          comment: '',
-          note: '')
+          comment: '')
     ];
     const comms = [
       ExternalCommInfo(
@@ -117,12 +116,6 @@ void main() {
     expect(find.text('ドメインの送信元検証設定'), findsOneWidget);
     expect(find.text('GeoIP解析：通信先の国別リスクチェック'), findsOneWidget);
     expect(find.text('LAN内デバイス一覧とリスクチェック'), findsOneWidget);
-    expect(
-        find.text(
-            'LAN 内に接続されているデバイス情報を取得し,\n'
-            'リスクレベルを可視化します。未管理や脆弱な端末が存在すると,\n'
-            'ネットワーク全体が攻撃にさらされる可能性があります。'),
-        findsOneWidget);
     expect(find.text('外部通信の暗号化状況'), findsOneWidget);
     expect(find.text('端末の防御機能の有効性チェック'), findsOneWidget);
     expect(find.text('Windows バージョン'), findsOneWidget);
@@ -137,24 +130,21 @@ void main() {
           vendor: 'A',
           name: 'D1',
           status: 'warning',
-          comment: '',
-          note: ''),
+          comment: ''),
       LanDeviceRisk(
           ip: '1.1.1.2',
           mac: '00:22',
           vendor: 'B',
           name: 'D2',
           status: 'danger',
-          comment: '',
-          note: ''),
+          comment: ''),
       LanDeviceRisk(
           ip: '1.1.1.3',
           mac: '00:33',
           vendor: 'C',
           name: 'D3',
           status: 'safe',
-          comment: '',
-          note: ''),
+          comment: ''),
     ];
 
     await tester.pumpWidget(
@@ -191,32 +181,28 @@ void main() {
           vendor: 'v',
           name: 'n1',
           status: 'safe',
-          comment: '',
-          note: ''),
+          comment: ''),
       LanDeviceRisk(
           ip: '2',
           mac: 'm2',
           vendor: 'v',
           name: 'n2',
           status: 'warning',
-          comment: '',
-          note: ''),
+          comment: ''),
       LanDeviceRisk(
           ip: '3',
           mac: 'm3',
           vendor: 'v',
           name: 'n3',
           status: 'danger',
-          comment: '',
-          note: ''),
+          comment: ''),
       LanDeviceRisk(
           ip: '4',
           mac: 'm4',
           vendor: 'v',
           name: 'n4',
           status: 'danger',
-          comment: '',
-          note: ''),
+          comment: ''),
     ];
 
     await tester.pumpWidget(
