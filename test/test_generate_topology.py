2f0d1a37d2500028aad69fb605a10629d3f97050
viiiviii538 <viiiviii538@gmail.com>
2025-08-15 11:36:04 +0900

    test: cover multiple paths and ellipse shape

--- test/test_generate_topology.py
+++ test/test_generate_topology.py
@@ -17,6 +17,46 @@ class GenerateTopologyTest(unittest.TestCase):
         self.assertIn("192.168.1.2", src)
         self.assertIn('LAN -- "192.168.1.2"', src)
 
+    def test_build_graph_with_paths(self):
+        data = {
+            "hosts": [
+                {
+                    "ip": "192.168.1.3",
+                    "hostname": "Host",
+                    "vendor": "V",
+                    "paths": [["LAN", "Switch1"]],
+                }
+            ]
+        }
+        g = generate_topology.build_graph(data)
+        src = g.source
+        self.assertIn('LAN -- Switch1', src)
+        self.assertIn('Switch1 -- "192.168.1.3"', src)
+        self.assertIn('label="192.168.1.3\nHost\nV"', src)
+
+    def test_build_graph_multiple_paths_and_shape(self):
+        data = {
+            "hosts": [
+                {
+                    "ip": "192.168.1.4",
+                    "hostname": "Host2",
+                    "vendor": "Vendor2",
+                    "paths": [
+                        ["LAN", "SwitchA", "RouterA"],
+                        ["LAN", "SwitchB"],
+                    ],
+                }
+            ]
+        }
+        g = generate_topology.build_graph(data)
+        src = g.source
+        self.assertIn('LAN -- SwitchA', src)
+        self.assertIn('SwitchA -- RouterA', src)
+        self.assertIn('RouterA -- "192.168.1.4"', src)
+        self.assertIn('LAN -- SwitchB', src)
+        self.assertIn('SwitchB -- "192.168.1.4"', src)
+        self.assertIn('node [shape=ellipse]', src)
+
     def test_save_graph_dot(self):
         g = generate_topology.Graph("test")
         with tempfile.TemporaryDirectory() as tmpdir:
